c33f0d0c475a45f81b10661f345465466239d2ae
Amy Shi <amyhshi92@gmail.com>
2022-03-09 12:38:07 -0500

    remove extra reloads, tweak CSS

--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ const wishlistOverlay = document.querySelector("#wishlist-overlay");
 
 let currentListName;
 let openWishlist = false;
+let booksInWish = []; // wishlist tracker: keep track of books in wishlist by title
 
 // loading initial wishlist
 fetch("http://localhost:3000/wishlist")
 .then(response => response.json())
 .then(books => books.forEach(book => {
+  // populate wishlist tracker;
+  booksInWish.push(book.title);
   const li = document.createElement("li");
   const title = toTitleCase(book.title);
   const br = document.createElement("br");
@@ -31,10 +34,10 @@ fetch("http://localhost:3000/wishlist")
   removeBtn.className = "custom-button";
   removeBtn.id = "removeBtn";
   removeBtn.addEventListener("click", () => {
+    const heart = document.querySelector(`#heart-${book.rank}`);
+    heart.textContent = "♡";
+    heart.classList = "open-heart";
     deleteBook(book, li);
-    // reload thumbnails so hearts refresh
-    booksContainer.innerHTML = "";
-    loadBooks(currentListName);
   }); 
   li.append(title, br, detailsBtn, buyBtn, removeBtn);
   document.querySelector("#booklist").append(li);
@@ -59,36 +62,30 @@ function loadBooks(listName) {
 }
 
 function loadThumbnail(book) {
-  let exists2;
+  let exists = false;
   const thumbnailCard = document.createElement("div");
   thumbnailCard.className = "thumbnail";
   thumbnailCard.style.backgroundImage = `url("${book.book_image}")`;
   const thumbnailRank = document.createElement("span");
   thumbnailRank.textContent = `#${book.rank}`;
-  thumbnailRank.id = "rank";
+  thumbnailRank.className = "rank";
   const heart = document.createElement("span");
-  // make a fetch request + find if find true then heart textContent = red
-  fetch("http://localhost:3000/wishlist")
-  .then(response => response.json())
-  .then(books => {
-    const exists = books.find(dbBook => dbBook.title === book.title);
-    exists2 = exists;
-    if (exists) {
-      heart.textContent = "♥";
-      heart.classList = "full-heart";
-    } else { 
-      heart.textContent = "♡" ;
-      heart.classList = "open-heart";
-    }
-  })
-  heart.className = "heart";
+  if (booksInWish.find(element => element === book.title)) {
+    heart.textContent = "♥";
+    heart.classList = "full-heart";
+    exists = true;
+  } else {
+    heart.textContent = "♡" ;
+    heart.classList = "open-heart";
+  }
+  heart.id = `heart-${book.rank}`;
   const br = document.createElement("br");
-  thumbnailCard.addEventListener("click", () => showDetails(book, heart, exists2));
+  thumbnailCard.addEventListener("click", () => showDetails(book));
   thumbnailCard.append(thumbnailRank, heart, br);
   booksContainer.append(thumbnailCard);
 }
 
-function showDetails(book, heart, exists) {
+function showDetails(book) {
   detailsContainer.innerHTML = "";
   const detailsInnerContainer = document.createElement("div");
   detailsInnerContainer.classList = "details-inner-container d-flex inline";
@@ -107,12 +104,12 @@ function showDetails(book, heart, exists) {
   bookImg.className = "big-image";
   const bookTitle = document.createElement("h2");
   bookTitle.textContent = toTitleCase(book.title);
-  const bookAuthor = document.createElement("h4");
+  const bookAuthor = document.createElement("h5");
   bookAuthor.textContent = `Author: ${book.author}`;
   const bookDescription = document.createElement("p");
   bookDescription.textContent = book.description;
   // show existing comments
-  const commentsHeading = document.createElement("h4");
+  const commentsHeading = document.createElement("h6");
   commentsHeading.textContent = "Comments:";
   commentsHeading.style.fontWeight = "bold";
   const commentList = document.createElement("ul");
@@ -127,9 +124,9 @@ function showDetails(book, heart, exists) {
       commentList.append(li);
     })
   })
-  //add comment form
-  // const commentDiv = document.createElement("div");
-  // commentDiv.classList = "d-flex flex-row align-items-center justify-content-center mt-3 mb-3";
+  // add comment form
+  const commentFormDiv = document.createElement("div");
+  commentFormDiv.classList = "d-flex flex-row align-items-center justify-content-center mt-3 mb-3";
   const commentForm = document.createElement("form");
   commentForm.id = "comment-form";
   const commentInput = document.createElement("textarea");
@@ -146,12 +143,11 @@ function showDetails(book, heart, exists) {
     addComment(book, commentList, commentForm.elements[0].value);
     commentForm.reset();
   })
-  // commentFormDiv.append(commentForm);
-
+  commentFormDiv.append(commentForm);
+  // make wishlist add button + functionality depending on if book is already in wishlist
   const addBtn = document.createElement("button");
-  addBtn.className = "custom-button";
-  addBtn.id = "addToWish"
-  if (exists) {
+  addBtn.classList = "custom-button add-to-wish";
+  if (booksInWish.find(bookItem => bookItem === book.title)) {
     addBtn.textContent = "Already in wishlist"
     addBtn.style.background = "rgba(93, 189, 206)";
     addBtn.style.color = "white";
@@ -160,10 +156,10 @@ function showDetails(book, heart, exists) {
     addBtn.textContent = "Add to wishlist";
     addBtn.style.background = "white";
     addBtn.style.color = "black";
-    addBtn.addEventListener("click", () => addToWishlist(book, heart));
+    addBtn.addEventListener("click", () => addToWishlist(book));
   }
   leftContainer.append(bookImg, addBtn);
-  rightContainer.append(bookTitle, bookAuthor, bookDescription, commentsHeading, commentList, commentForm);
+  rightContainer.append(bookTitle, bookAuthor, bookDescription, commentsHeading, commentList, commentFormDiv);
   detailsInnerContainer.append(exitBtn, leftContainer, rightContainer);
   detailsContainer.append(detailsInnerContainer);
   detailsContainer.style.display = "block";
@@ -189,14 +185,16 @@ function addComment(book, commentList, comment) {
   })
 }
 
-function addToWishlist(book, heart) {
+function addToWishlist(book) {
+  // add book to wishlist tracker
+  booksInWish.push(book.title);
+  console.log(booksInWish);
+  const heart = document.querySelector(`#heart-${book.rank}`);
   heart.textContent = "♥";
-  heart.classList = "full-heart"
+  heart.classList = "full-heart";
   openWishlist = true;
-  //overlay slide
-  wishlistOverlay.style.right = "350px";
+  wishlistOverlay.style.width = "350px";
   hideDetails();
-
   const li = document.createElement("li");
   const title = toTitleCase(book.title);
   const br = document.createElement("br");
@@ -204,12 +202,13 @@ function addToWishlist(book, heart) {
   detailsBtn.className = "custom-button";
   detailsBtn.id = "detailsBtn";
   detailsBtn.textContent = "Details";
-  detailsBtn.addEventListener("click", () => showDetails(book, heart, "true"));
+  detailsBtn.addEventListener("click", () => showDetails(book));
   const buyBtn = document.createElement("button");
   buyBtn.textContent = "Buy";
   buyBtn.className = "custom-button";
   buyBtn.id = "buyBtn";
   buyBtn.addEventListener("click", () => showBuyLinks(book));
+  // remove button + functionality
   const removeBtn = document.createElement("button");
   removeBtn.textContent = "Remove";
   removeBtn.className = "custom-button";
@@ -218,10 +217,10 @@ function addToWishlist(book, heart) {
   li.append(title, br, detailsBtn, buyBtn, removeBtn);
   document.querySelector("#booklist").append(li);
 
-  postToDatabase(book, li, heart, removeBtn);
+  postToDatabase(book, li, removeBtn);
 }
 
-function postToDatabase(book, li, heart, removeBtn) {
+function postToDatabase(book, li, removeBtn) {
   fetch("http://localhost:3000/wishlist", {
     method: "POST",
     headers: {
@@ -240,15 +239,13 @@ function postToDatabase(book, li, heart, removeBtn) {
     )
   })
   .then(response => response.json())
-  .then(book => {
+  .then(savedBook => {
     removeBtn.addEventListener("click", () => {
-    heart.textContent = "♡";
-    heart.style.color = "black";
-    deleteBook(book, li);
+      const heart = document.querySelector(`#heart-${book.rank}`);
+      heart.textContent = "♡";
+      heart.classList = "open-heart";
+      deleteBook(savedBook, li);
     });
-    // reload thumbnails so hearts/addbtn refresh
-    booksContainer.innerHTML = "";
-    loadBooks(currentListName);
   })
 }
 
@@ -279,6 +276,11 @@ function showBuyLinks(book) {
 }
 
 function deleteBook(book, li) {
+  // remove book from wishlist tracker
+  const index = booksInWish.indexOf(book.title);
+  console.log(index)
+  booksInWish.splice(index, 1);
+  console.log(booksInWish);
   li.remove();
   fetch(`http://localhost:3000/wishlist/${book.id}`, {
     method: "DELETE",
@@ -287,9 +289,6 @@ function deleteBook(book, li) {
       Accept: "application/json"
     }
   })
-  // reload thumbnails so hearts/addbtn refresh
-  booksContainer.innerHTML = "";
-  loadBooks(currentListName);
 }
 
 function hideDetails() {
@@ -309,12 +308,10 @@ function toTitleCase(str) {
 // OPEN/CLOSE WISHLIST - CART ICON
 document.querySelector("#wishlist-icon").addEventListener("click", () => toggleWishlist())
 function toggleWishlist() {
-  // openWishlist = !openWishlist;
+  openWishlist = !openWishlist;
   if (openWishlist) {
-    wishlistOverlay.style.width = "0";
-    openWishlist = false;
+    wishlistOverlay.style.width = "350px";
   } else { 
-    wishlistOverlay.style.width = "350px"
-    openWishlist = true;
+    wishlistOverlay.style.width = "0px";
   }
 }
